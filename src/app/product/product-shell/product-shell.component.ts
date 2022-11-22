a4f979be3d6bfb2335a2d53b48c7c82feae43e4e
xin.gu <xin.gu@accenture.com>
2022-11-22 16:09:19 +0100

    add product detail

---
+++ src/app/product/product-shell/product-shell.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-product-shell',
+  templateUrl: './product-shell.component.html',
+  styles: [
+  ]
+})
+export class ProductShellComponent {
+
+  constructor() { }
+
+}
